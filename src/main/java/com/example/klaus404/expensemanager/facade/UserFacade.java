19f391345b038b5101d5da1319ab72eeb2cd81e7
Klaus404 <barbuclaudiu404@gmail.com>
2024-04-30 17:08:08 +0300

    Added classes for implementing the user auth logic

---
+++ src/main/java/com/example/klaus404/expensemanager/facade/UserFacade.java
@@ -0,0 +1,6 @@
+package com.example.klaus404.expensemanager.facade;
+
+public class UserFacade {
+
+    //TODO: implement facade class for user save to database and authentication
+}
---
+++ src/main/java/com/example/klaus404/expensemanager/service/ClaimsService.java
@@ -0,0 +1,5 @@
+package com.example.klaus404.expensemanager.service;
+
+public class ClaimsService {
+    //TODO: implement logic for translating from uit to db
+}
